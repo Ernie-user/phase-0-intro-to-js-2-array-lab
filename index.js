065cd68da994bc524e319fe0aeba0c8f52e93c9a
Ernar Kozhebekov <erkodv@gmail.com>
2024-07-02 18:59:51 -0600

    Added functions to index.js

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+  }
+
+  function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+  }
+
+  function appendCat(name) {
+    const catsPlusCat = [...cats, name];
+    return catsPlusCat
+  } 
+
+  function prependCat(name) {
+    const catPlusCats = [name, ...cats];
+    return catPlusCats
+  } 
+
+  function removeLastCat() {
+    const noLastCat = cats.slice(0, -1);
+    return noLastCat
+  } 
+
+  function removeFirstCat() {
+    const noFirstCat = cats.slice(1);
+    return noFirstCat
+  } 
